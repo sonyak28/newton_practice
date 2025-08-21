1716bd3cec9260357ce0a31ea2240ee00185e104
Sonya <sonyak@berkeley.edu>
2025-08-21 09:39:49 -0700

    everything from main

--- newton.py
+++ newton.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def first_derivative(f, x, small_h= 10**-5):
+def first_derivative(x, f, small_h= 10**-5):
     """ Takes the first derivative 
 
     Parameters:
@@ -10,7 +10,7 @@ def first_derivative(f, x, small_h= 10**-5):
     """
     return (f(x + small_h) - f(x) ) / small_h
 
-def second_derivative(f, x, small_h=10**-5):
+def second_derivative(x, f, small_h=10**-5):
     """ Takes the second derivative 
 
     Parameters:
@@ -18,9 +18,9 @@ def second_derivative(f, x, small_h=10**-5):
     x - the same integer that is passed in as starting_value into optimize()
     small_h - the same optional parameter that is passed into optimize(); default number is 10**-5
     """    
-    return (first_derivative(f, x + small_h, small_h) - first_derivative(f, x, small_h)) / small_h
+    return (first_derivative(x + small_h, f, small_h) - first_derivative(x, f, small_h)) / small_h
 
-def optimize(f, starting_value, small_h=10**-5):
+def optimize(starting_value, f, small_h=10**-5):
     """ Function for Newton’s method for optimization
 
     Parameters:
@@ -28,13 +28,16 @@ def optimize(f, starting_value, small_h=10**-5):
     starting values - an int (ex: 10)
     small_h - optional parameter; default number is 10**-5
     """
-    max_iterations = 10
-    history = [starting_value]
-    for i in np.arange(max_iterations):
-        if (history[-1] - starting_value) < 0.5 and (history[-1] != starting_value):
-            break
-        new_x_t = starting_value - ((first_derivative(f, starting_value, small_h)) /  second_derivative(f, starting_value, small_h))
-        history.append(new_x_t)
-        starting_value = new_x_t
-    return starting_value, history
+    # max_iterations = 10
+    
+    if not isinstance(starting_value, (int, float, np.number)):
+        raise TypeError("`x0` must be numeric")
+
+    new_x_t = starting_value - ((first_derivative(starting_value, f, small_h)) /  second_derivative(starting_value, f, small_h))
+    x= starting_value
+    # starting_value = new_x_t
+    while abs(x - new_x_t) > small_h:
+        x = new_x_t
+        new_x_t = x - ((first_derivative(x, f, small_h)) /  second_derivative(x, f, small_h))
+    return {'x': new_x_t, 'value': f(new_x_t)}
 
